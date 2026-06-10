f3bae766f3e294d95ba98d27436bcf113aaa5fb9
Andreart135 <a.obregon24@info.uas.edu.mx>
2026-06-10 09:40:33 -0700

    Add files via upload

---
+++ Iterfaz.py
@@ -0,0 +1,14 @@
+
+    # 1. Mensaje inicial uwu
+    print("𖹭" * 60)
+    print("\n  ⁺‧₊˚RECOMENDACIONES DE LIBROS DE GUTENBERG˚₊‧⁺˖   \n")
+    print("𖹭" * 60)
+    print("Este programa analiza libros para darte resumenes")
+    print("o recomendarte libros similares basados en tus gustos.\n")
+    print("𖹭" * 60)
+    directorio_libros = "libros_txt"
+
+
+
+
+
